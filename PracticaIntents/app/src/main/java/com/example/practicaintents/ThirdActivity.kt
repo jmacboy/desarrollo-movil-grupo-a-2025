e4cb01a352aa84ef6251d07de16e1319a55b620b
Jose Alvarez <JoseMiguel.Alvarez@jalasoft.com>
2025-03-10 11:43:22 -0400

    added class of 2025-10-03 and 2025-10-07

---
+++ PracticaIntents/app/src/main/java/com/example/practicaintents/ThirdActivity.kt
@@ -0,0 +1,25 @@
+package com.example.practicaintents
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var lblWelcomeThird: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_third)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        lblWelcomeThird = findViewById(R.id.lblWelcomeThird)
+        val name = intent.getStringExtra("clientName")
+        lblWelcomeThird.text = getString(R.string.welcome_text, name)
+    }
+}
