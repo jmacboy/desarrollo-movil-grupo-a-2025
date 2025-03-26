e9f9e7b331358d654eef7c3c9daee0a72c46849e
Jose Alvarez <JoseMiguel.Alvarez@jalasoft.com>
2025-03-26 12:03:58 -0400

    Updated to the class of 2025-03-26

---
+++ PracticaRoomCrud/app/src/main/java/com/example/practicaroom/repositories/RoomRepository.kt
@@ -0,0 +1,14 @@
+package com.example.practicaroom.repositories
+
+import android.content.Context
+import androidx.room.Room
+import com.example.practicaroom.db.AppDatabase
+
+object RoomRepository {
+    fun getRoomInstance(context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java, "prueba-db"
+        ).build()
+    }
+}
