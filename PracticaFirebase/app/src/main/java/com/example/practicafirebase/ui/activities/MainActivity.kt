64f3df5f4fa0bc881d148807ca2142130784e651
Jose Alvarez <JoseMiguel.Alvarez@jalasoft.com>
2025-04-09 12:24:19 -0400

    updated to class of 2025-04-09

---
+++ PracticaFirebase/app/src/main/java/com/example/practicafirebase/ui/activities/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.practicafirebase.ui.activities
+
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practicafirebase.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        askNotificationPermission()
+        getFirebaseToken()
+
+    }
+
+    private fun getFirebaseToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("TOKEN", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("TOKEN", token)
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
+    }
+}
--- PracticaGeolocalizacion/app/src/main/java/com/example/practicageolocalizacion/MapsActivity.kt
+++ PracticaGeolocalizacion/app/src/main/java/com/example/practicageolocalizacion/MapsActivity.kt
@@ -3,6 +3,7 @@ package com.example.practicageolocalizacion
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
@@ -24,7 +25,10 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -32,14 +36,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
-
+    private var currentMarkerCount = 1
+    private var isLocationUpdating = false
     private val locationRequest = LocationRequest.Builder(
-        Priority.PRIORITY_BALANCED_POWER_ACCURACY,
-        1000
+        Priority.PRIORITY_HIGH_ACCURACY,
+        5000
     )
-        .setWaitForAccurateLocation(false)
-        .setMaxUpdates(1)
+        .setMinUpdateDistanceMeters(20f)
+        .setMinUpdateIntervalMillis(5000)
         .build()
+    private var polyline: Polyline? = null
+    private val loadedLocations: ArrayList<LatLng> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,12 +75,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun onLocationUpdated(locationResult: LocationResult) {
         for (location in locationResult.locations) {
             Log.d("Location", "Location updated: ${location.latitude}, ${location.longitude}")
+            val theLatLng = LatLng(location.latitude, location.longitude)
             mMap.addMarker(
                 MarkerOptions().position(
-                    LatLng(location.latitude, location.longitude)
-                ).title("You are here!")
+                    theLatLng
+                ).title("Marcador $currentMarkerCount")
             )
+            currentMarkerCount++
+
+            loadedLocations.add(theLatLng)
+            refreshPolylines()
+            zoomToAllMarkers()
         }
+    }
+
+    private fun zoomToAllMarkers() {
+        val builder = LatLngBounds.Builder()
+        for (location in loadedLocations) {
+            builder.include(location)
+        }
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                builder.build(), 150
+            )
+        )
     }
 
     private fun setupEventListeners() {
@@ -81,7 +106,56 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             getLastLocation()
         }
         binding.btnUpdateLocationContinuous.setOnClickListener {
-            startLocationUpdates()
+            var buttonText = "Actualizar la ubicación periódicamente"
+            if (isLocationUpdating) {
+                stopLocationUpdates()
+                isLocationUpdating = false
+            } else {
+                startLocationUpdates()
+                isLocationUpdating = true
+                buttonText = "Detener Actualización"
+            }
+
+            binding.btnUpdateLocationContinuous.text = buttonText
+
+        }
+        binding.btnShowCurrentLocation.setOnClickListener {
+            showCurrentLocation()
+        }
+    }
+
+    private fun showCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                ),
+                REQUEST_CURRENT_LOCATION
+            )
+            return
+        }
+        fusedLocationClient.getCurrentLocation(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            null
+        ).addOnSuccessListener { location: Location? ->
+            if (location == null) {
+                return@addOnSuccessListener
+            }
+            val latLng = LatLng(location.latitude, location.longitude)
+            mMap.addMarker(MarkerOptions().position(latLng).title("You are here!"))
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        }.addOnFailureListener {
+            Toast.makeText(
+                this,
+                "No se pudo obtener la ubicación actual",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -106,7 +180,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 arrayOf(
                     Manifest.permission.ACCESS_FINE_LOCATION,
                 ),
-                3
+                REQUEST_LOCATION_PERIODICAL
             )
             return
         }
@@ -114,6 +188,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             locationRequest,
             locationCallback,
             Looper.getMainLooper()
+        )
+    }
+
+    private fun refreshPolylines() {
+        if (polyline != null) {
+            polyline?.remove()
+        }
+        polyline = mMap.addPolyline(
+            PolylineOptions().apply {
+                width(2f)
+                color(Color.RED)
+                add(*loadedLocations.toTypedArray())
+            }
+
         )
     }
 
@@ -129,7 +217,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 arrayOf(
                     Manifest.permission.ACCESS_FINE_LOCATION,
                 ),
-                2
+                REQUEST_LAST_LOCATION
             )
             return
         }
@@ -157,6 +245,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
@@ -173,7 +263,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 arrayOf(
                     Manifest.permission.ACCESS_FINE_LOCATION,
                 ),
-                1
+                REQUEST_LOCATION_MAP
             )
             return
         }
@@ -186,46 +276,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode == 1) {
-            if (grantResults.isNotEmpty() &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED
-            ) {
-                mMap.isMyLocationEnabled = true
-            } else {
-                Toast.makeText(
-                    this,
-                    "La app necesita permisos de ubicación para funcionar",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-            }
-        } else if (requestCode == 2) {
-            if (grantResults.isNotEmpty() &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED
-            ) {
-                getLastLocation()
-            } else {
-                Toast.makeText(
-                    this,
-                    "La app necesita permisos de ubicación para funcionar",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-            }
-        } else if (requestCode == 3) {
-            if (grantResults.isNotEmpty() &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED
-            ) {
-                startLocationUpdates()
-            } else {
-                Toast.makeText(
-                    this,
-                    "La app necesita permisos de ubicación para funcionar",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-            }
+        if (requestCode == REQUEST_LOCATION_MAP && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+        } else if (requestCode == REQUEST_LAST_LOCATION && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            getLastLocation()
+        } else if (requestCode == REQUEST_LOCATION_PERIODICAL && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            startLocationUpdates()
+        } else if (
+            requestCode == REQUEST_CURRENT_LOCATION && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            showCurrentLocation()
+        } else {
+            Toast.makeText(
+                this,
+                "La app necesita permisos de ubicación para funcionar",
+                Toast.LENGTH_SHORT
+            )
+                .show()
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION_MAP = 1
+        private const val REQUEST_LAST_LOCATION = 2
+        private const val REQUEST_LOCATION_PERIODICAL = 3
+        private const val REQUEST_CURRENT_LOCATION = 4
     }
 }
